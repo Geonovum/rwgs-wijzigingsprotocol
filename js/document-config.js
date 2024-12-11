4589e517d89f9dea983396791652120fedd066e4
lvdbrink <l.vandenbrink@geonovum.nl>
2024-12-11 11:34:07 +0100

    correct thisVersion

--- js/document-config.js
+++ js/document-config.js
@@ -10,9 +10,6 @@ var documentConfig =
     "https://docs.geostandaarden.nl/serv/wijzigingsprotocol/"
   ],
   publishDate: "2024-05-28",
-  thisVersion: [
-    "https://geonovum.github.io/serv-wijzigingsprotocol/"
-  ],
   github: "https://github.com/geonovum/rwgs-wijzigingsprotocol/",
   issueBase: "https://github.com/geonovum/rwgs-wijzigingsprotocol/issues/",
   edDraftURI: "https://geonovum.github.io/rwgs-wijzigingsprotocol/",
